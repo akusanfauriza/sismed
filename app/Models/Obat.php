e62b58dbca124ae6792aeb5aa0986736433670ec
akusanfauriza <yafiatfacebook@gmail.com>
2024-12-27 21:10:15 +0700

    fix bug tidak bisa query table obat, karena bentuk table nya tidak plural

--- app/Models/Obat.php
+++ app/Models/Obat.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Obat extends Model
 {
     use HasFactory;
+    // Tentukan nama tabel yang benar
+    protected $table = 'obat';
+
+    // Kolom yang dapat diisi secara massal
+    protected $fillable = [
+        'nama_obat',
+        'jenis_obat',
+        'dosis',
+        'stok',
+        'harga',
+        'keterangan',
+    ];
 }
